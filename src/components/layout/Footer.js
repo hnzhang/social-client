2ab4a91a0580adf8c069ab6fbc7178e4ca286ec0
Harry Zhang <hnzhang@gmail.com>
2018-09-12 09:29:21 -0700

    static layout setup

---
+++ src/components/layout/Footer.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default () => {
+  return (
+    <footer className="bg-dark text-white mt-5 p-4 text-center">
+      Hello Developers, Happy in {new Date().getFullYear()} with the Social Network!
+    </footer>
+  );
+};
